48c14d7dddfc423dab0b352d542782913d2a3895
Byan Nur Cahyo <byannurcahyo9@gmail.com>
2024-03-12 22:56:40 +0700

    EloquentORM

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+//use App\Models\User;
+use Illuminate\Http\Request;
+use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index() {
+        // $data = [
+        //     'username' => 'customer-1',
+        //     'nama' => 'Pelanggan',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 4
+        // ];
+        // UserModel::insert($data);
+
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
+
+        // tambah data user dengan Eloquent Model
+        $data = [
+            'nama' => 'Pelanggan Pertama',
+        ];
+        UserModel::where('username', 'customer-1')->update($data); // update data user
+
+        // coba akss model userModel
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+    }
+}
