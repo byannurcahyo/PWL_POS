37765107712bb29b87d7daf08830c3dda940984c
Byan Nur Cahyo <byannurcahyo9@gmail.com>
2024-04-03 14:23:20 +0700

    LayoutingAdminLTE

--- database/migrations/2024_04_03_015830_m_user_table.php
+++ database/migrations/2024_04_03_015830_m_user_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\m_user;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -7,18 +8,23 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        //
+    * Run the migrations.
+    */
+    public function up(): void {
+        Schema::create('useri', function (Blueprint $table) {
+            $table-> id('user_id');
+            $table -> unsignedBigInteger('level_id')->index;
+            $table ->string ('username', 20)->unique();
+            $table ->string ('nama', 100);
+            $table ->string ('password');
+            $table ->timestamps();
+            $table->foreign('level_id')-> references('level_id')->on('m_level');
+        });
     }
-
     /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        //
+    * Reverse the migrations.
+    */
+    public function down(): void {
+        Schema::dropIfExists('useri');
     }
 };
