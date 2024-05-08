3112f47662b791ad60044b3cff5a596861b5ba7c
Byan Nur Cahyo <byannurcahyo9@gmail.com>
2024-05-08 11:17:49 +0700

    RestfulApiRegister

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -8,15 +8,28 @@ use App\Models\LevelModel;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class UserModel extends Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
-    use HasFactory;
-
-    protected $table = 'm_user'; // Mendefisikan nama tabel yang digunakan
-    protected $primaryKey = 'user_id'; // Mendefinisikan primary key dari tabel yang digunakan
-    protected $fillable = ['level_id', 'username', 'nama', 'password']; // Mendefinisikan kolom yang dapat diisi dengan data
-    public function level(): BelongsTo {
-        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    public function getJWTIdentifier() {
+        return $this->getKey();
     }
+
+    public function getJWTCustomClaims() {
+        return [];
+    }
+
+    protected $table = 'm_user';
+    protected $primaryKey = 'user_id';
+    protected $guarded = [];
+
+    // use HasFactory;
+
+    // protected $table = 'm_user'; // Mendefisikan nama tabel yang digunakan
+    // protected $primaryKey = 'user_id'; // Mendefinisikan primary key dari tabel yang digunakan
+    // protected $fillable = ['level_id', 'username', 'nama', 'password']; // Mendefinisikan kolom yang dapat diisi dengan data
+    // public function level(): BelongsTo {
+    //     return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    // }
 }
